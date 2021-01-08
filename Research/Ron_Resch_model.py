be9d4782b1ccf7036c91f9fe7e2fa073a3cc238c
BaekgyeomKim <kwe7541@gmail.com>
2021-01-08 22:14:54 +0900

    RR model(python version),Ron_Resch_model.py

---
+++ Research/Ron_Resch_model.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+alpha_1 = np.arange(0,np.pi,np.pi/1000)
+alpha_1_1 = np.arange(0,np.pi,np.pi/100)
+alpha_2 = np.arange(2/3*np.pi,np.pi,np.pi/3000)
+
+R = 25.
+# print(R)
+# print(type(R))
+
+theta = 2.*np.arcsin(np.)
