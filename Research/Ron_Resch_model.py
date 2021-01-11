d2a0b1219b83cc92f4e6ce7f9370f3648ad6a2a7
BaekgyeomKim <kwe7541@gmail.com>
2021-01-11 15:27:40 +0900

    modify Ron_Resch_model.py

--- Research/Ron_Resch_model.py
+++ Research/Ron_Resch_model.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-## input variable
+## input variables
 alpha_1 = np.arange(0*np.pi, np.pi,  np.pi/1800)
 alpha_1 = np.append(alpha_1,np.pi)
 alpha_2 = np.arange(2/3*np.pi , np.pi, np.pi/5400)
@@ -18,9 +18,10 @@ print(theta.shape)
 eta = np.arccos((np.sqrt(2*(1-np.cos(alpha_2)))-np.sin(theta/2))/(np.sqrt(3)*np.cos(theta/2)))
 print(eta.shape)
 
-## coordinate of the point 24
+## coordinates of the point 24
 X_24 = np.array([])
 Y_24 = np.array([])
+
 for i in range(1801):
     if np.sqrt(3)*np.tan(theta[i]/2) > np.cos(eta[i]):
         x_24 = np.sqrt(3)*R*np.cos(eta[i])*np.cos(theta[i]/2)
@@ -39,13 +40,75 @@ for i in range(1801):
     else:
         y_24 = R/2*(np.cos(eta[i])*np.cos(theta[i]/2)+np.sqrt(3)*np.sin(theta[i]/2))
         Y_24 = np.append(Y_24, y_24)
+print(Y_24)
+print(Y_24.shape)
 
-z_24 = R*np.cos(theta/2)*np.sin(eta)
+Z_24 = R*np.cos(theta/2)*np.sin(eta)
 
 
-##
+## coordinates of the point 14
+
+X_14 = R/2*(np.sqrt(3)*np.cos(eta)*np.cos(theta/2)+np.sin(theta/2))
+Y_14 = R/2*np.abs(np.sqrt(3)*np.sin(theta/2)-np.cos(eta)*np.cos(theta/2))
+Z_14 = Z_24
+
+## coordinates of the point 22
+
+X_22 = np.array([])
+Y_22 = np.array([])
+
+for i in range(1801):
+    if np.sqrt(3)*np.tan(theta[i]/2) > np.cos(eta[i]):
+        x_22 = R*np.sin(theta[i]/2)
+        X_22 = np.append(X_22, x_22)
+    else:
+        x_22 = R/2*(np.sqrt(3)*np.cos(eta[i])*np.cos(theta[i]/2)-np.sin(theta[i]/2))
+        X_22 = np.append(X_22, x_22)
+
+print("X_22 shape is", X_22.shape)
+
+for i in range(1801):
+    if np.sqrt(3)*np.tan(theta[i]/2) > np.cos(eta[i]):
+        y_22 = R*np.cos(eta[i])*np.cos(theta[i]/2)
+        Y_22 = np.append(Y_22, y_22)
+    else:
+        y_22 = R/2*(np.cos(eta[i])*np.cos(theta[i]/2)+np.sqrt(3)*np.sin(theta[i]/2))
+        Y_22 = np.append(Y_22, y_22)
+
+print("Y_22 shape is", Y_22.shape)
+
+Z_22 = Z_24
+
+print("Z_22 shape is", Z_22.shape)
+
+## coordinates of the point 13
+
+X_13 = np.zeros((1801,))
+print("X_13 shape is", X_13.shape)
+
+Y_13 = np.zeros((1801,))
+Z_13 = np.zeros((1801,))
+
+## coordinates of the point 15
+
+X_15 = R*(np.sqrt(3)*np.cos(eta)*np.cos(theta/2)+np.sin(theta/2))
+Y_15 = np.zeros((1801,))
+Z_15 = np.zeros((1801,))
+
+## coordinates of the point 23
+
+X_23 = X_14
+Y_23 = np.sqrt(3)/6*X_15
+Z_23 = -np.sqrt(np.power(np.sqrt(3)/3*2*R, 2)-np.power(X_23, 2)-np.power(Y_23, 2))
 
 
 ## plot
-plt.plot(alpha_1, theta)
+plt.figure(1)
+plt.plot(alpha_1, X_24)
+plt.plot(alpha_1, X_23)
+
+plt.figure(2)
+plt.plot(alpha_1, Z_24)
+plt.plot(alpha_1, Z_23)
+
 plt.show()
