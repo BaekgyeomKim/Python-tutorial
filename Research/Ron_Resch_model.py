fee795f027497c2b5ffc67ea6cb41970af843a9f
baekgyeomkim <kwe7541@gmail.com>
2021-01-18 19:32:39 +0900

    add curvature plot Ron_Resch_model.py

--- Research/Ron_Resch_model.py
+++ Research/Ron_Resch_model.py
@@ -7,7 +7,8 @@ from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
 ## colormap setting
 viridis = cm.get_cmap('viridis', 18)
-
+plasma = cm.get_cmap('plasma', 6)
+cividis = cm.get_cmap('cividis', 6)
 
 ## input variables
 alpha_1 = np.arange(0*np.pi, np.pi,  np.pi/1800)
@@ -144,11 +145,43 @@ d = np.sqrt(np.power(X_14 - X_22, 2) + np.power(Y_14 - Y_22, 2))
 A = np.sqrt(np.power(X_23, 2) + np.power(Y_23, 2))
 B = A
 C = np.power(Z_14, 2)*np.power(A, 2)/(X_15*X_23 + 2*Y_14*Y_23 - (np.power(X_15, 2))/4 - (np.power(Y_14, 2)))
-6
+
 u = np.linspace(0.0, 2.0*np.pi, 60)
 v = np.linspace(0.0, 1.0/2.0*np.pi, 30)
 t = np.linspace(0.0, np.pi, 30)
 
+xx1 = A[100, ]*np.cos(t)
+yy1 = C[100, ]*np.sin(t)
+xx2 = A[200, ]*np.cos(t)
+yy2 = C[200, ]*np.sin(t)
+xx3 = A[300, ]*np.cos(t)
+yy3 = C[300, ]*np.sin(t)
+xx4 = A[400, ]*np.cos(t)
+yy4 = C[400, ]*np.sin(t)
+xx5 = A[500, ]*np.cos(t)
+yy5 = C[500, ]*np.sin(t)
+xx6 = A[600, ]*np.cos(t)
+yy6 = C[600, ]*np.sin(t)
+
+x_zero = np.zeros((30,1))
+
+## radius of curvature at point 13 and 24
+
+def curvature(a,c,x,z):
+    kappa = 1/(np.power(a, 2)*np.power(c, 2))*np.power(np.power(x, 2)/np.power(a, 4) + np.power(z, 2)/np.power(c, 4), -3./2.)
+    return kappa
+
+k_p24 = np.array([])
+k_p13 = np.array([])
+
+for i in range(1801):
+    k24 = curvature(A[i, ], C[i, ], X_24[i, ], Z_24[i, ])
+    k_p24 = np.append(k_p24, k24)
+
+print(k_p24.shape)
+print(k_p13.shape)
+print(X_13.shape)
+print(alpha_1d.shape)
 
 ## plot
 # plt.figure(1)
@@ -164,21 +197,24 @@ t = np.linspace(0.0, np.pi, 30)
 # plt.figure(2)
 # plt.plot(alpha_1d, Z_24)
 # plt.plot(alpha_1d, Z_23)
-
-plt.figure(3)
-triangles = [[0, 1, 2]]
-triang = np.array([])
-Triang = np.array([])
-for i in range (18):
-    triang = mtri.Triangulation([P_22[100*i, 0], P_24[100*i, 0], P_14[100*i, 0]],
-                                   [P_22[100*i, 1], P_24[100*i, 1], P_14[100*i, 1]], triangles)
-    Triang = np.append(Triang, triang)
-
-for i in range (18):
-    plt.triplot(Triang[i,], c=viridis(i/18))
-
+#
+# plt.figure(3)
+# triangles = [[0, 1, 2]]
+# triang = np.array([])
+# Triang = np.array([])
+# for i in range (18):
+#     triang = mtri.Triangulation([P_22[100*i, 0], P_24[100*i, 0], P_14[100*i, 0]],
+#                                    [P_22[100*i, 1], P_24[100*i, 1], P_14[100*i, 1]], triangles)
+#     Triang = np.append(Triang, triang)
+#
+# for i in range (18):
+#     plt.triplot(Triang[i,], c=viridis(i/18))
+#
+# plt.grid(color='lightgray', linestyle='--')
+# plt.xlabel('x position')
+# plt.ylabel('y position')
+#
 ## Point 24, 22, 14의 위치를 원점이 Point 23이 기준이 되도록 이동시킨다.
-##
 
 plt.figure(4)
 x = A[100, ]*np.outer(np.cos(u), np.sin(v))
@@ -186,7 +222,11 @@ y = B[100, ]*np.outer(np.sin(u), np.sin(v))
 z = C[100, ]*np.outer(np.ones_like(u), np.cos(v))
 ax = Axes3D(plt.figure(4))
 ax.plot_surface(x, y, z, cmap='plasma', alpha=0.4)
+ax.plot(x_zero, xx1, yy1, color='k')
 ax.set_zlim(0, 30)
+ax.set_xlabel('x position')
+ax.set_ylabel('y position')
+ax.set_zlabel('z position')
 
 plt.figure(5)
 x = A[300, ]*np.outer(np.cos(u), np.sin(v))
@@ -194,29 +234,31 @@ y = B[300, ]*np.outer(np.sin(u), np.sin(v))
 z = C[300, ]*np.outer(np.ones_like(u), np.cos(v))
 ax = Axes3D(plt.figure(5))
 ax.plot_surface(x, y, z, cmap='viridis', alpha=0.4)
+ax.plot(x_zero, xx3, yy3, color='k')
 ax.set_zlim(0, 30)
+ax.set_xlabel('x position')
+ax.set_ylabel('y position')
+ax.set_zlabel('z position')
 
 plt.figure(6)
-xx1 = A[100, ]*np.cos(t)
-yy1 = C[100, ]*np.sin(t)
-xx2 = A[200, ]*np.cos(t)
-yy2 = C[200, ]*np.sin(t)
-xx3 = A[300, ]*np.cos(t)
-yy3 = C[300, ]*np.sin(t)
-xx4 = A[400, ]*np.cos(t)
-yy4 = C[400, ]*np.sin(t)
-xx5 = A[500, ]*np.cos(t)
-yy5 = C[500, ]*np.sin(t)
-xx6 = A[600, ]*np.cos(t)
-yy6 = C[600, ]*np.sin(t)
-plt.plot(xx1, yy1)
-plt.plot(xx2, yy2)
-plt.plot(xx3, yy3)
-plt.plot(xx4, yy4)
-plt.plot(xx5, yy5)
-plt.plot(xx6, yy6)
-plt.xlim(-40,40)
-plt.ylim(0,40)
+plt.plot(xx1, yy1, c='k')
+plt.plot(xx2, yy2, c='k')
+plt.plot(xx3, yy3, c='k')
+plt.plot(xx4, yy4, c='k')
+plt.plot(xx5, yy5, c='k')
+plt.plot(xx6, yy6, c='k')
+plt.xlabel("x position", fontsize=12)
+plt.ylabel("z position", fontsize=12)
+plt.xlim(-40, 40)
+plt.ylim(0, 40)
 plt.grid(color='lightgray', linestyle='--')
+
+plt.figure(7)
+plt.plot(alpha_1d, k_p24, color='r')
+plt.xlabel(r'Dihedral angle, $\alpha_1$ [$\degree$]')
+plt.ylabel(r'Radius of curvature, $\kappa$ [1/mm]')
+plt.grid(color='lightgray', linestyle='--')
+plt.xlim(0, 180)
+plt.ylim(0, 0.16)
 
 plt.show()
